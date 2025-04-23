8a2de2d0786cf5c20f8e2612fe087d2149ee0d90
gabicastrum <gabriela.laurindo@dbserver.com.br>
2025-04-23 15:57:33 -0300

    initial commit: projeto realizado com o curso RxJS na Alura

---
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Item, Livro } from 'src/app/models/interface';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { LivroService } from 'src/app/service/livro.service';
+
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css'],
+})
+export class ListaLivrosComponent implements OnDestroy {
+  listaLivros: Livro[];
+  campoBusca: string = '';
+  subscription: Subscription;
+  livro: Livro;
+
+  constructor(private service: LivroService) {}
+
+  buscarLivros() {
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: (items) => {
+        this.listaLivros = this.livrosResultadoParaLivros(items);
+      },
+      error: (erro) => console.error(erro), //opcional, emitidas uma vez
+      complete: () => console.log('Observable completado'), //opcional, emitidas uma vez, pode apagar
+    });
+  }
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map((item) => {
+      return new LivroVolumeInfo(item);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe(); //desinscreve do observable
+  }
+}
